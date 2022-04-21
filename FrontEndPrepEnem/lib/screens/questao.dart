9c7299fc44bbb4f6e5a602722d823197118d80ed
Adriano Fantinelli <56205037+adriano-fantinelli@users.noreply.github.com>
2022-04-20 23:06:05 -0300

    Delete FrontEndPrepEnem directory

--- FrontEndPrepEnem/lib/screens/questao.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Questao extends StatelessWidget {
-  const Questao({Key? key}) : super(key: key);
-
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
